7f03c25208d8bc6632f810ac267315f2a45244f9
Johnny Max <johnnymax_10@hotmail.com>
2025-05-15 10:34:03 -0300

    Codigo para verificar updates e codigo para inicializar

--- codigos/inicia.sh
+++ codigos/inicia.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
+# Exibe a mensagem no console
+echo "Bem-vindo ao seu WSL2! Vamos começar o trabalho."
 
-# Definindo cores para o texto
-VERDE='\033[0;32m'
-AZUL='\033[0;34m'
-NEGRITO='\033[1m'
-RESET='\033[0m'
-
-# Sua frase personalizada - modifique como desejar
-MENSAGEM="${VERDE}${NEGRITO}Bem-vindo ao seu WSL2! Vamos começar o trabalho.${RESET}"
-
-# Exibe a mensagem no console e a salva em um arquivo de log
-echo -e "$MENSAGEM"
-
-# Opcionalmente, envia a mensagem para todos os terminais conectados
-echo -e "$MENSAGEM" | wall
