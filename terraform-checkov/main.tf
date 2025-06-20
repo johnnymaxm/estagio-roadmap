79597211b610bdf725e818defec364a1fb8703d7
johnnymax <johnnnymax_10@hotmail.com>
2025-06-20 13:15:19 -0300

    testando checkov

---
+++ terraform-checkov/main.tf
@@ -0,0 +1,49 @@
+# Criar a VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vpc-teste"
+  }
+}
+
+# Criar uma Subnet pública
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "vpc-teste-public-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "vpc-teste-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "vpc-teste-public-rt"
+  }
+}
+
+# Associar Route Table com a Subnet
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
