d21c9b17c3bfa5f0844340aac9cbb6b89d2cff38
johnnymax <johnnnymax_10@hotmail.com>
2025-06-23 10:00:38 -0300

    testando checkov

--- terraform-checkov/main.tf
+++ terraform-checkov/main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.subnet_cidr
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "vpc-teste-public-subnet"
